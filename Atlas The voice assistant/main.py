dbe8623b3919d9dab677b69248c3c7529e81349b
IamNayanKumarHar <itsnayankumarhar@gmail.com>
2024-08-16 12:56:04 +0530

    Majour fixes on voice command and url opener

--- Atlas The voice assistant/main.py
+++ Atlas The voice assistant/main.py
@@ -6,25 +6,6 @@ import pygame
 
 assistant = "atlas"
 
-# url library
- 
-youtube = "https://www.youtube.com"
-google = "https://www.google.com"
-spotify = "https://www.spotify.com"
-gmail = "https://www.gmail.com"
-flipkart = "https://www.flipkart.com"
-
-# song library
-
-deva = "https://www.youtube.com/watch?v=mNuhKUOD_A0"
-malang = "https://www.youtube.com/watch?v=UbMgcdmYC70"
-grateful = "https://www.youtube.com/watch?v=83RUhxsfLWs"
-careless = "https://www.youtube.com/watch?v=Z6L4u2i97Rw&list=RD83RUhxsfLWs&index=2"
-summer = "https://www.youtube.com/watch?v=KxwUy2S2n-Q"
-dreams = "https://www.youtube.com/watch?v=8Ps9fSF4qcQ"
-aria = "https://www.youtube.com/watch?v=onIWoZzpp7I"
-
-
 def palying_music(path):
     pygame.mixer.init()
     pygame.mixer.music.load(path)
@@ -38,25 +19,17 @@ def speak(speech):
     engine.runAndWait()
 
 def url_opener(instructions):
-    if instructions.lower() == "youtube" or instructions.lower() == "open youtube" :
-        webbrowser.open(youtube)
-    elif instructions.lower().startswith("open"):
+    if instructions.lower().startswith("open"):
         web = instructions.lower().split(" ")[1]
-        if web in globals():
-            webbrowser.open(globals()[web])
-        else:
-            print(f"{web} not found.")
+        webbrowser.open(f"https://www.{web}.com")
 
-    elif instructions.lower().startswith("play"):
-        song = instructions.lower().split(" ")[1] 
-        if song in globals():
-            webbrowser.open(globals()[song])
-        else:
-            print(f"{song} not found.")
-    
-    elif instructions.lower().endswith("on spotify") and instructions.lower().startswith("play"):
+    elif instructions.lower().startswith("play") and instructions.lower().endswith("on youtube"):  
+        video = instructions.lower().split(" ")[1]
+        webbrowser.open(f"https://www.youtube.com/results?search_query={video}")    
+        
+    elif instructions.lower().startswith("play") and instructions.lower().endswith("on spotify"):  
         music = instructions.lower().split(" ")[1]
-        webbrowser.open(f"https://open.spotify.com/search/{instructions}")
+        webbrowser.open(f"https://open.spotify.com/search/{music}")
 
 def play(filename):
     pygame.mixer.init(frequency=16000)
@@ -96,32 +69,23 @@ if __name__ == "__main__":
     print(f"Activating {assistant}...")
     speak(f" Activating {assistant}...")
     while True:
-        play('Atlas The voice assistant/music/level-up-191997.mp3')
-        wake_word = taking_voice_to_wake_assistant()
-    
         try:
+            play('Atlas The voice assistant/music/level-up-191997.mp3')
+            wake_word = taking_voice_to_wake_assistant()
+    
             if wake_word.lower() == f"hello {assistant}" or wake_word.lower() == f"hey {assistant}":
                 play('Atlas The voice assistant/music/level-up-191997.mp3')
-                speak(f"Hello, I am {assistant}. How may I help you?")
+                print(f"Hello, I am {assistant}. How may I help you?")
 
                 play('Atlas The voice assistant/music/notification-beep-229154.mp3')
                 instructions = taking_voice_to_get_instructions()
                 url_opener(instructions)
 
-
-        except sr.UnknownValueError:
-            print("Sorry your voice is noise or may you are away from the mic please speak clearly")
-            speak("Sorry your voice is noise or may you are away from the mic please speak clearly")
-            continue
-
-        except sr.RequestError as e:
-            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
-            speak("Please check your internet connection..")
-            continue
-
-        except KeyboardInterrupt:
+        except Exception as e:
             pass
             continue
+
+        
 
 
     
